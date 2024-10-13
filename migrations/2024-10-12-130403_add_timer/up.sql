5de6f623a50cb2cc6ee8652a0ba37521c0436d05
DanCodes <dan@dancodes.online>
2024-10-13 15:10:20 +0100

    chore: update migration

--- migrations/2024-10-12-130403_add_timer/up.sql
+++ migrations/2024-10-12-130403_add_timer/up.sql
@@ -1,2 +1,1 @@
-ALTER TABLE scan_events ADD COLUMN can_process TIMESTAMP NOT NULL;
-UPDATE scan_events SET can_process = created_at;
+ALTER TABLE scan_events ADD COLUMN can_process TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;
