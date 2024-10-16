5cb0307171a3de1eb6a8249b181742f648086638
DanCodes <dan@dancodes.online>
2024-10-16 16:14:44 +0100

    fix(logs): file layering + no ansi

--- src/utils/logs.rs
+++ src/utils/logs.rs
@@ -1,14 +1,21 @@
 use anyhow::{Context, Ok};
-use std::path::PathBuf;
+use std::{io, path::PathBuf};
 use tracing_appender::non_blocking::WorkerGuard;
+use tracing_subscriber::{fmt, layer::SubscriberExt, EnvFilter};
 
 pub fn setup_logs(
     log_level: String,
     log_file: Option<PathBuf>,
 ) -> anyhow::Result<Option<WorkerGuard>> {
-    let filter = format!("autopulse={},actix_web=info", log_level);
+    let collector = tracing_subscriber::registry()
+        .with(
+            EnvFilter::default()
+                .add_directive(format!("autopulse={log_level}").parse()?)
+                .add_directive("actix_web=info".parse()?),
+        )
+        .with(fmt::Layer::new().with_writer(io::stdout));
 
-    let log_tracer = tracing_subscriber::fmt().with_env_filter(filter);
+    let mut file_guard = None;
 
     if let Some(log_file) = log_file {
         let writer = std::fs::OpenOptions::new()
@@ -19,12 +26,17 @@ pub fn setup_logs(
 
         let (non_blocking, guard) = tracing_appender::non_blocking(writer);
 
-        log_tracer.with_writer(non_blocking).init();
+        file_guard = Some(guard);
 
-        return Ok(Some(guard));
-    }
+        let collector =
+            collector.with(fmt::Layer::new().with_ansi(false).with_writer(non_blocking));
 
-    log_tracer.init();
+        tracing::subscriber::set_global_default(collector)
+            .expect("Unable to set a global subscriber");
+    } else {
+        tracing::subscriber::set_global_default(collector)
+            .expect("Unable to set a global subscriber");
+    };
 
-    Ok(None)
+    Ok(file_guard)
 }
