e7654810b84cb6b4147389032e7ca63436c23236
DanCodes <dan@dancodes.online>
2024-10-17 01:09:10 +0100

    fix(logs): local timestamp (#65)

--- src/utils/logs.rs
+++ src/utils/logs.rs
@@ -1,19 +1,26 @@
 use anyhow::{Context, Ok};
 use std::{io, path::PathBuf};
 use tracing_appender::non_blocking::WorkerGuard;
+use tracing_subscriber::fmt::time::OffsetTime;
 use tracing_subscriber::{fmt, layer::SubscriberExt, EnvFilter};
 
 pub fn setup_logs(
     log_level: String,
     log_file: Option<PathBuf>,
 ) -> anyhow::Result<Option<WorkerGuard>> {
+    let timer = OffsetTime::local_rfc_3339()?;
+
     let collector = tracing_subscriber::registry()
         .with(
             EnvFilter::default()
                 .add_directive(format!("autopulse={log_level}").parse()?)
                 .add_directive("actix_web=info".parse()?),
         )
-        .with(fmt::Layer::new().with_writer(io::stdout));
+        .with(
+            fmt::Layer::new()
+                .with_timer(timer.clone())
+                .with_writer(io::stdout),
+        );
 
     let mut file_guard = None;
 
@@ -28,8 +35,12 @@ pub fn setup_logs(
 
         file_guard = Some(guard);
 
-        let collector =
-            collector.with(fmt::Layer::new().with_ansi(false).with_writer(non_blocking));
+        let collector = collector.with(
+            fmt::Layer::new()
+                .with_timer(timer)
+                .with_ansi(false)
+                .with_writer(non_blocking),
+        );
 
         tracing::subscriber::set_global_default(collector)
             .expect("Unable to set a global subscriber");
