b78dbd66628365ab779385ba7d5107b32b1dc153
DanCodes <dan@dancodes.online>
2025-02-07 15:10:16 +0000

    chore(nix): use flake over shell w/ `crane`

---
+++ flake.nix
@@ -0,0 +1,179 @@
+{
+  description = "Build a cargo project";
+
+  inputs = {
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    crane.url = "github:ipetkov/crane";
+
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.rust-analyzer-src.follows = "";
+    };
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    advisory-db = {
+      url = "github:rustsec/advisory-db";
+      flake = false;
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      crane,
+      fenix,
+      flake-utils,
+      advisory-db,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        inherit (pkgs) lib;
+
+        craneLib = crane.mkLib pkgs;
+
+        sqlFilter = path: _type: builtins.match ".*sql$" path != null;
+        mdFilter = path: _type: builtins.match ".*md$" path != null;
+        cssFilter = path: _type: builtins.match ".*css$" path != null;
+        filter =
+          path: type:
+          (cssFilter path type) || (mdFilter path type) || (sqlFilter path type) || (craneLib.filterCargoSources path type);
+
+        src = pkgs.lib.cleanSourceWith {
+          src = ./.;
+          filter = filter;
+          name = "source";
+        };
+
+        features = [
+          "sqlite"
+          "postgres"
+          # "mysql"
+        ];
+
+        commonArgs = {
+          inherit src;
+          strictDeps = true;
+
+          nativeBuildInputs = [
+            pkgs.pkg-config
+          ];
+          buildInputs =
+            [
+              pkgs.openssl
+              pkgs.sqlite
+              pkgs.postgresql
+            ]
+            ++ pkgs.lib.optionals (pkgs.lib.elem "sqlite" features) [
+              pkgs.libmysqlclient
+              pkgs.ncurses
+            ]
+            ++ lib.optionals pkgs.stdenv.isDarwin [
+              pkgs.libiconv
+            ];
+        };
+
+        craneLibLLvmTools = craneLib.overrideToolchain (
+          fenix.packages.${system}.complete.withComponents [
+            "cargo"
+            "llvm-tools"
+            "rustc"
+          ]
+        );
+
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+        crate = craneLib.buildPackage (
+          commonArgs
+          // {
+            inherit cargoArtifacts;
+          }
+        );
+      in
+      {
+        checks = {
+          # Build the crate as part of `nix flake check` for convenience
+          inherit crate;
+
+          # Run clippy (and deny all warnings) on the crate source,
+          # again, reusing the dependency artifacts from above.
+          #
+          # Note that this is done as a separate derivation so that
+          # we can block the CI if there are issues here, but not
+          # prevent downstream consumers from building our crate by itself.
+          clippy = craneLib.cargoClippy (
+            commonArgs
+            // {
+              inherit cargoArtifacts;
+              cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+            }
+          );
+
+          doc = craneLib.cargoDoc (
+            commonArgs
+            // {
+              inherit cargoArtifacts;
+            }
+          );
+
+          # Check formatting
+          fmt = craneLib.cargoFmt {
+            inherit src;
+          };
+
+          # Audit dependencies
+          audit = craneLib.cargoAudit {
+            inherit src advisory-db;
+          };
+
+          # Run tests with cargo-nextest
+          # Consider setting `doCheck = false` on `crate` if you do not want
+          # the tests to run twice
+          nextest = craneLib.cargoNextest (
+            commonArgs
+            // {
+              inherit cargoArtifacts;
+              partitions = 1;
+              partitionType = "count";
+              cargoNextestPartitionsExtraArgs = "--no-tests=pass";
+            }
+          );
+        };
+
+        packages =
+          {
+            default = crate;
+          }
+          // lib.optionalAttrs (!pkgs.stdenv.isDarwin) {
+            crate-llvm-coverage = craneLibLLvmTools.cargoLlvmCov (
+              commonArgs
+              // {
+                inherit cargoArtifacts;
+              }
+            );
+          };
+
+        apps.default = flake-utils.lib.mkApp {
+          drv = crate;
+        };
+
+        devShells.default = craneLib.devShell {
+          checks = self.checks.${system};
+
+          inputsFrom = [ crate ];
+
+          packages = [
+            pkgs.biome
+            pkgs.nixfmt-rfc-style
+          ];
+        };
+      }
+    );
+}
