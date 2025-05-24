638dcc7d93ab2d6ffcc2fd3345e7030def5ca0f5
DanCodes <dan@dancodes.online>
2025-05-24 12:48:43 +0100

    feat(timers): add event-specific timers to -rr triggers

--- crates/service/src/settings/triggers/mod.rs
+++ crates/service/src/settings/triggers/mod.rs
@@ -219,19 +219,33 @@ impl Trigger {
         }
     }
 
-    pub const fn get_timer(&self) -> &Timer {
+    pub fn get_timer(&self, event_name: Option<String>) -> &Timer {
         match &self {
-            Self::Sonarr(trigger) => &trigger.timer,
-            Self::Radarr(trigger) => &trigger.timer,
-            Self::Lidarr(trigger) => &trigger.timer,
-            Self::Readarr(trigger) => &trigger.timer,
+            Self::Sonarr(trigger) => event_name
+                .as_ref()
+                .and_then(|event| trigger.event_timers.get(event))
+                .unwrap_or(&trigger.timer),
+            Self::Radarr(trigger) => event_name
+                .as_ref()
+                .and_then(|event| trigger.event_timers.get(event))
+                .unwrap_or(&trigger.timer),
+            Self::Lidarr(trigger) => event_name
+                .as_ref()
+                .and_then(|event| trigger.event_timers.get(event))
+                .unwrap_or(&trigger.timer),
+            Self::Readarr(trigger) => event_name
+                .as_ref()
+                .and_then(|event| trigger.event_timers.get(event))
+                .unwrap_or(&trigger.timer),
             Self::Manual(trigger) => &trigger.timer,
             Self::Notify(trigger) => &trigger.timer,
         }
     }
 
-    pub fn paths(&self, body: serde_json::Value) -> anyhow::Result<Vec<(String, bool)>> {
-        match &self {
+    pub fn paths(&self, body: serde_json::Value) -> anyhow::Result<(String, Vec<(String, bool)>)> {
+        let event_name = body["eventType"].as_str().unwrap_or("unknown").to_string();
+
+        let paths = match &self {
             Self::Sonarr(_) => Ok(SonarrRequest::from_json(body)?.paths()),
             Self::Radarr(_) => Ok(RadarrRequest::from_json(body)?.paths()),
             Self::Lidarr(_) => Ok(LidarrRequest::from_json(body)?.paths()),
@@ -239,7 +253,9 @@ impl Trigger {
             Self::Manual(_) | Self::Notify(_) => {
                 Err(anyhow::anyhow!("Manual trigger does not have paths"))
             }
-        }
+        }?;
+
+        Ok((event_name, paths))
     }
 
     pub const fn excludes(&self) -> &Vec<String> {
