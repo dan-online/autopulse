bd3b6df2aecb7b7ac5bc21caf4e7ff645fb66700
DanCodes <dan@dancodes.online>
2025-06-22 20:24:01 +0800

    feat(rewrite): enhance rewrite to support multiple rewrites

--- crates/utils/src/rewrite.rs
+++ crates/utils/src/rewrite.rs
@@ -1,8 +1,57 @@
 use regex::Regex;
 use serde::Deserialize;
 
-#[derive(Deserialize, Clone)]
+/// Rewrites
+///
+/// This struct allows for a flexible way to define path rewrites using regex.
+///
+/// It can handle both single and multiple rewrites, where each rewrite consists of a `from` regex pattern and a `to` replacement string.
+///
+/// ```yml
+/// rewrite:
+///   from: '^/old/path/(.*)$'
+///   to: '/new/path/$1'
+/// ```
+/// or
+/// ```yml
+/// rewrite:
+///   - from: /testing
+///     to: /production
+///   - from: '^/old/path/(.*)$'
+///     to: '/new/path/$1'
+/// ``````
+#[derive(Clone)]
 pub struct Rewrite {
+    pub(crate) rewrites: Vec<SingleRewrite>,
+}
+
+impl<'de> Deserialize<'de> for Rewrite {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        #[derive(Deserialize)]
+        #[serde(untagged)]
+        enum Inner {
+            Single(SingleRewrite),
+            Multiple(Vec<SingleRewrite>),
+        }
+
+        let inner = Inner::deserialize(deserializer)?;
+
+        let rewrites = match inner {
+            Inner::Single(single) => {
+                vec![single]
+            }
+            Inner::Multiple(multiple) => multiple,
+        };
+
+        Ok(Rewrite { rewrites })
+    }
+}
+
+#[derive(Deserialize, Clone)]
+pub struct SingleRewrite {
     /// Path to rewrite from
     pub from: String,
     /// Path to rewrite to
@@ -11,9 +60,36 @@ pub struct Rewrite {
 
 impl Rewrite {
     pub fn rewrite_path(&self, path: String) -> String {
-        let from_regex = Regex::new(self.from.as_str()).expect("Invalid regex in 'from' field");
-        let result = from_regex.replace(&path, self.to.as_str()).to_string();
+        let mut result = path;
+
+        for rewrite in &self.rewrites {
+            let from_regex = Regex::new(&rewrite.from).expect("Invalid regex in 'from' field");
+            result = from_regex.replace_all(&result, &rewrite.to).to_string();
+        }
 
         result
+    }
+
+    #[cfg(test)]
+    pub fn single(from: &str, to: &str) -> Self {
+        Rewrite {
+            rewrites: vec![SingleRewrite {
+                from: from.to_string(),
+                to: to.to_string(),
+            }],
+        }
+    }
+
+    #[cfg(test)]
+    pub fn multiple(rewrites: Vec<(&str, &str)>) -> Self {
+        Rewrite {
+            rewrites: rewrites
+                .into_iter()
+                .map(|(from, to)| SingleRewrite {
+                    from: from.to_string(),
+                    to: to.to_string(),
+                })
+                .collect(),
+        }
     }
 }
