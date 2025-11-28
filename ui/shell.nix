d5e11a2e535b762e09d5a8ffea1605dd9ca22b72
DanCodes <dan@dancodes.online>
2025-11-28 15:19:13 +0000

    feat(nix): allow shell.nix

---
+++ ui/shell.nix
@@ -0,0 +1,11 @@
+# TODO: migrate to flake and build dockerfile with nix
+{
+  pkgs ? import <nixpkgs> { },
+}:
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    nodejs
+    yarn-berry
+    biome
+  ];
+}
