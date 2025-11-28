8e305c7f65162852defb279799d17f56e5fa0f04
DanCodes <dan@dancodes.online>
2025-11-28 15:51:31 +0000

    feat(docker): migrate ui to nix based images

---
+++ ui/flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "autopulse UI shell";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs
+            yarn-berry
+          ];
+        };
+      }
+    );
+}
