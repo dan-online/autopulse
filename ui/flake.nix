0219b5b8f9a65679a9a3e47ae95b8ebd82c7518c
DanCodes <dan@dancodes.online>
2026-05-25 11:11:32 +0100

    fix(ui): use corepack instead of yarn-berry for version-accurate yarn

--- ui/flake.nix
+++ ui/flake.nix
@@ -22,7 +22,7 @@
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             nodejs
-            yarn-berry
+            corepack
           ];
         };
       }
